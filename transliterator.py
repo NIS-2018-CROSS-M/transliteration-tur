4d9eb462177e042cbc526ef92b1487834c2c0e2d
Oleg Serikov <srkvoa@gmail.com>
2019-03-09 22:56:46 +0300

    Create transliterator.py

---
+++ transliterator.py
@@ -0,0 +1,18 @@
+class Transliterator:
+    src_to_tgt_chars_table = {}
+    def __init__(self, transliteration_table_fn):
+        self.src_to_tgt_chars_table = {}
+        with open(transliteration_table_fn, 'r', encoding="utf-8") as table_f:
+            for line in table_f:
+                translit_pair = line.rstrip().split()
+                if len(translit_pair) != 2:
+                    continue
+                self.src_to_tgt_chars_table[translit_pair[0]] = translit_pair[1]
+                
+    def transliterate(self, line):
+        res = []
+        for c in line:
+            if c in self.src_to_tgt_chars_table.keys():
+                if self.src_to_tgt_chars_table[c] != '0':
+                    res.append(self.table[c])
+        return ''.join(res)
